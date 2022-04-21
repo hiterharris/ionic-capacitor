47b10e767093c17ec82c07cf3c45595c8fd7ed9e
Hiter Harris <hiterharris@comcast.net>
2022-04-20 22:32:51 -0400

    set up ionic capacitor, run app on iPhone simulator

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hhIonicCapacitor.app',
+  appName: 'ionic-capacitor',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
